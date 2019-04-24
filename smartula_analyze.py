98e17de89155af077eb51326f3be22fe30f7fa5e
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2019-04-24 17:18:16 +0200

    Added simple plotting.

--- smartula_analyze.py
+++ smartula_analyze.py
@@ -1,5 +1,8 @@
 import getopt
 import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from smartula_ask import SmartulaAsk
 
@@ -7,11 +10,12 @@ from smartula_ask import SmartulaAsk
 def main(argv):
     username = ''
     password = ''
+    file_name = ''
 
     try:
-        opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password="])
+        opts, args = getopt.getopt(argv, "hu:p:f:", ["username=", "password=", "file="])
     except getopt.GetoptError:
-        print('smartula_analyze.py -u <username> -p <password>')
+        print('smartula_analyze.py [-u <username> -p <password>] [--f <path_to_file>]')
         sys.exit(2)
 
     for opt, arg in opts:
@@ -22,10 +26,20 @@ def main(argv):
             username = arg
         elif opt in ("-p", "--password"):
             password = arg
+        elif opt in ("-f", "--file"):
+            file_name = arg
 
-    sma = SmartulaAsk(username, password, "http://cejrowskidev.com:8884/")
-    samples = sma.get_sound(1300001, 119)
-    print(samples)
+    if file_name:
+        # Read CSV
+        samples = pd.read_csv(file_name)
+        print(samples.head())
+        samples.plot()
+        plt.show()
+
+    else:
+        sma = SmartulaAsk(username, password, "http://cejrowskidev.com:8884/")
+        samples = sma.get_sound(1300001, 119)
+        print(samples)
 
 
 if __name__ == "__main__":
