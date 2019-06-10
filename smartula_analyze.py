f241c3aaeb3556c1be1de0eb48630120e913e875
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2019-06-10 14:51:44 +0200

    Clean up.

--- smartula_analyze.py
+++ smartula_analyze.py
@@ -11,6 +11,82 @@ from smartula_ask import SmartulaAsk
 from smartula_sound import SmartulaSound
 
 
+def main(argv):
+    username = ''
+    password = ''
+    file_name = ''
+    sound = 0
+
+    try:
+        opts, args = getopt.getopt(argv, "hu:p:s:f:", ["username=", "password=", "sound=", "file="])
+    except getopt.GetoptError:
+        print('smartula_analyze.py [-u <username> -p <password> -s <sound_id>] [--f <path_to_file>]')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('smartula_analyze.py -u <username> -p <password>')
+            sys.exit()
+        elif opt in ("-u", "--username"):
+            username = arg
+        elif opt in ("-s", "--sound"):
+            sound = arg
+        elif opt in ("-p", "--password"):
+            password = arg
+        elif opt in ("-f", "--file"):
+            file_name = arg
+
+    if file_name:
+        # Read CSV
+        samples = read_from_csv(file_name)
+        sms = SmartulaSound(samples)
+        sms.get_fft()
+        print('End!')
+    elif username and password:
+        sma = SmartulaAsk(username, password, "http://cejrowskidev.com:8884/")
+
+        # Create folder for CSVs
+        try:
+            os.mkdir("csv")
+        except FileExistsError:
+            print("Folder already exists.")
+
+        # Get actual samples
+        try:
+            sound_id = int(sound)
+            __get_sound_and_save_to_file(sma, sound_id)
+        except ValueError:
+            sound_ids = __parse_range_string(sound)
+            for sound_id in sound_ids:
+                __get_sound_and_save_to_file(sma, sound_id)
+    else:
+        # Analyze whole csv folder
+        print("Smartula analyze start!")
+        __prepare_mfcc_in_folder("csv/", "mfcc-electromagnetic-field")
+
+        print("Smartula analyze end!")
+
+
+def __prepare_mfcc_in_folder(folder_with_sounds, mfcc_folder_name):
+    """
+    Function for reading sounds from specific folder and extracting stationary mfcc values
+    :type folder_with_sounds: str   Folder name where sound should be placed
+    :type mfcc_folder_name: str     Folder where mfccs will be saved
+    """
+    list_of_audios = __change_directory_prepare_sound_with_mfcc(folder_with_sounds,
+                                                                [("2019-06-04T18-22-00", "2019-06-04T20-30-00"),
+                                                                 ("2019-06-05T20-46-00", "2019-06-05T23-48-00"),
+                                                                 ("2019-06-06T22-23-00", "2019-06-07T05-52-00")])
+    try:
+        os.mkdir(mfcc_folder_name)
+    except FileExistsError:
+        print("Folder " + mfcc_folder_name + " already exists.")
+    for smartula_audio in list_of_audios:
+        save_to_file(mfcc_folder_name + "/" +
+                     str(smartula_audio.electromagnetic_field_on) + smartula_audio.timestamp,
+                     smartula_audio.mfcc_feature_vector)
+
+
 def save_to_file(filename, samples):
     """
     Function for saving samples in csv file
@@ -75,74 +151,6 @@ def __change_directory_prepare_sound_with_mfcc(folder_name, list_of_tuples_inter
                                     __affected(f.replace(".csv", ""), list_of_tuples_interval))
                       for f in all_filenames]
     return list_of_audios
-
-
-def main(argv):
-    username = ''
-    password = ''
-    file_name = ''
-    sound = 0
-
-    try:
-        opts, args = getopt.getopt(argv, "hu:p:s:f:", ["username=", "password=", "sound=", "file="])
-    except getopt.GetoptError:
-        print('smartula_analyze.py [-u <username> -p <password> -s <sound_id>] [--f <path_to_file>]')
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print('smartula_analyze.py -u <username> -p <password>')
-            sys.exit()
-        elif opt in ("-u", "--username"):
-            username = arg
-        elif opt in ("-s", "--sound"):
-            sound = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-        elif opt in ("-f", "--file"):
-            file_name = arg
-
-    if file_name:
-        # Read CSV
-        samples = read_from_csv(file_name)
-        sms = SmartulaSound(samples)
-        sms.get_fft()
-        print('End!')
-    elif username and password:
-        sma = SmartulaAsk(username, password, "http://cejrowskidev.com:8884/")
-
-        # Create folder for CSVs
-        try:
-            os.mkdir("csv")
-        except FileExistsError:
-            print("Folder already exists.")
-
-        # Get actual samples
-        try:
-            sound_id = int(sound)
-            __get_sound_and_save_to_file(sma, sound_id)
-        except ValueError:
-            sound_ids = __parse_range_string(sound)
-            for sound_id in sound_ids:
-                __get_sound_and_save_to_file(sma, sound_id)
-    else:
-        # Analyze whole csv folder
-        print("Smartula analyze start!")
-        list_of_audios = __change_directory_prepare_sound_with_mfcc("csv/",
-                                                                    [("2019-06-04T18-22-00", "2019-06-04T20-30-00"),
-                                                                     ("2019-06-05T20-46-00", "2019-06-05T23-48-00"),
-                                                                     ("2019-06-06T22-23-00", "2019-06-07T05-52-00")])
-
-        try:
-            os.mkdir("mfcc-electromagnetic-field")
-        except FileExistsError:
-            print("Folder already exists.")
-        for smartula_audio in list_of_audios:
-            save_to_file("mfcc-electromagnetic-field/" +
-                         str(smartula_audio.electromagnetic_field_on) + " " + smartula_audio.timestamp,
-                         smartula_audio.mfcc_feature_vector)
-        print("We got " + str(len(list_of_audios)) + " audio samples")
-        print("Smartula analyze end!")
 
 
 if __name__ == "__main__":
