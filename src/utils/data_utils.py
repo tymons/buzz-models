966f8c9b6b7f2d19f7b450990ba82e0249dc2a42
Tymoteusz Cejrowski <tymtoeusz.cejrowski@smartula.eu>
2021-04-23 12:44:10 +0200

    Support for checking if point is in polygon.

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -186,3 +186,23 @@ def read_comet_api_key(config_file_fullpath):
         api_key = [b.split('=')[-1] for b in f.read().splitlines() if b.startswith('api_key')][0]
     
     return api_key
+
+def pointinpolygon(x,y,poly):
+    n = len(poly)
+    inside = False
+    p2x = 0.0
+    p2y = 0.0
+    xints = 0.0
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
